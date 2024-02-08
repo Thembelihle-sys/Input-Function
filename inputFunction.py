6551737b9550d8ec9a3e13b346e470cf2b29b519
Thembelihle-sys <141932567+Thembelihle-sys@users.noreply.github.com>
2024-02-08 21:25:46 +0200

    Add files via upload

---
+++ inputFunction.py
@@ -0,0 +1,9 @@
+print('Tell me anything')
+anything=input() 
+print('Hmm...', anything, '...Really?')
+
+#input() function with an argument
+
+anything =float(input('Enter a number')) 
+result = anything ** 2.0
+print(anything, 'to the power of 2 is: ', result)
